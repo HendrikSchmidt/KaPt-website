dc344d3c6e289d72293a0e2d46b0c78c4432a8ee
Hendrik Schmidt <hendriknas@gmail.com>
2022-11-14 13:59:08 +0100

    Update variables.js

--- src/lib/variables.js
+++ src/lib/variables.js
@@ -2,10 +2,9 @@ import { sluggify } from './utils';
 
 const dev = process.env.NODE_ENV === 'development';
 const basePath = 'http://localhost:1337';
-const apiPath = 'http://localhost:1337/api';
-// const apiPath = dev
-// 	? 'http://localhost:1337/api'
-// 	: 'https://pechtold-architekten-cms-production.up.railway.app/api';
+const apiPath = dev
+	? 'http://localhost:1337/api'
+	: 'https://kapt-cms-production.up.railway.app//api';
 
 const getSlugsToIdsMapping = async (route, fieldToGetSlugFrom) => {
 	const response = await fetch(`${apiPath}/${route}?pagination[pageSize]=100`);
