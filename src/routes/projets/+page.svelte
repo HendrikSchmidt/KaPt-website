0df88b15020bbd23d6b995bb2adef7bc6d13395f
Hendrik Schmidt <hendriknas@gmail.com>
2022-12-10 20:05:31 +0100

    make projects interactive

--- src/routes/projets/+page.svelte
+++ src/routes/projets/+page.svelte
@@ -19,18 +19,33 @@
       }
     };
   });
+  
+  const isActive = projects.map(() => false);
+
+  const makeActive = (index) => {
+    isActive[index] = !isActive[index];
+  }
+
 </script>
 
 {#each projects as project, index (project.id)}
-  <div class="my-24 flex flex-row justify-around">
-    <div class="w-3/12 pt-6">
-      <!-- <img src={project.attributes.Portrait.data.attributes.url} alt={biographie.Portrait.data.attributes.url} /> -->
+  <div class="py-12 flex flex-row justify-around border-b-2 border-black cursor-pointer" on:click={() => makeActive(index)}>
+    <div class="w-6/12 pr-6">
+      {#if isActive[index]}
+        {#each project.attributes.Images.data as img}
+          <img src={img.attributes.url} alt={img.attributes.alternativeText} />
+        {/each}
+      {:else}
+        <img src={project.attributes.Images.data[0].attributes.url} alt={project.attributes.Images.data[0].attributes.alternativeText} />
+      {/if}
     </div>
     <div class="w-6/12 prose-sm">
       <h2>{project.attributes.Nom}</h2>
       <p class="font-semibold">{project.attributes.Localisation}</p>
       <p class="font-semibold">{project.attributes.Surface} m<sup>2</sup></p>
-      <SvelteMarkdown source={project.attributes.shortDescription} options={variables.markdownOptions} />
+      {#if isActive[index]}
+        <SvelteMarkdown source={project.attributes.Description} options={variables.markdownOptions} />
+      {/if}
     </div>
   </div>
 {/each}
