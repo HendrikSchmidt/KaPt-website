c283df4e87945028c5a7acca1b1e95fe61544d5f
Hendrik Schmidt <hendriknas@gmail.com>
2022-12-10 19:00:14 +0100

    update basePath

--- src/lib/variables.js
+++ src/lib/variables.js
@@ -1,11 +1,6 @@
-import { sluggify } from './utils';
-
-const dev = process.env.NODE_ENV === 'development';
-const basePath = 'http://localhost:1337';
 const apiPath = 'https://kapt-cms-production.up.railway.app/api';
 
 export const variables = {
-	basePath,
 	apiPath,
 	markdownOptions: {
 		breaks: true,
--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,15 @@
 import adapter from '@sveltejs/adapter-static';
 import preprocess from "svelte-preprocess";
 
+const dev = process.env.NODE_ENV === 'development';
+
 const config = {
 	kit: {
 		adapter: adapter(),
 
 		files: {
 			assets: 'static',
+			basePath: dev ? '' : '/KaPt-website',
 		},
 	},
 	preprocess: [
