9aacdc629c1c58c56f58f78d2832f7417b1443df
Hendrik Schmidt <hendriknas@gmail.com>
2022-12-10 20:05:35 +0100

    Update +page.svelte

--- src/routes/philosophie/+page.svelte
+++ src/routes/philosophie/+page.svelte
@@ -14,7 +14,7 @@
   {#each philosophy.Biographies as biographie, index (biographie.id)}
     <div class="my-24 flex space-between relative">
       <div class="w-4/12 sticky top-0 self-start">
-        <img src={biographie.Portrait.data.attributes.url} alt={biographie.Portrait.data.attributes.url} />
+        <img src={biographie.Portrait.data.attributes.url} alt={biographie.Portrait.data.attributes.alternativeText} />
       </div>
       <div class="w-8/12 pl-6 inline">
         <h2>{biographie.Nom}</h2>
