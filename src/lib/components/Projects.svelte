b1098c2441ca002251095fb140a6a8e7f099da51
Hendrik Schmidt <hendriknas@gmail.com>
2022-12-18 20:32:54 +0100

    restyle project page

--- src/lib/components/Projects.svelte
+++ src/lib/components/Projects.svelte
@@ -24,24 +24,36 @@
 
 </script>
 
-{#each projects as project, index (project.id)}
-  <div class="py-12 flex flex-row justify-around border-b-2 border-black cursor-pointer" on:click={() => makeActive(index)}>
-    <div class="w-6/12 pr-6">
-      {#if isActive[index]}
-        {#each project.attributes.Images.data as img}
-          <img src={img.attributes.url} alt={img.attributes.alternativeText} />
-        {/each}
-      {:else}
-        <img src={project.attributes.Images.data[0].attributes.url} alt={project.attributes.Images.data[0].attributes.alternativeText} />
-      {/if}
+<div class="my-36">
+  {#each projects as project, index (project.id)}
+    <div class="pb-24 flex flex-col sm:flex-row justify-around cursor-pointer" on:click={() => makeActive(index)} on:keypress={() => makeActive(index)} >
+      
+      <div class="p-2 pr-4 sm:w-1/2 xl:w-5/12 prose prose-sm lg:prose-base">
+        <h2>{project.attributes.Nom}</h2>
+        <p class="font-semibold">{project.attributes.Programme}</p>
+        <p class="font-semibold">{project.attributes.Localisation}</p>
+        <p class="font-semibold">{project.attributes.Surface} m<sup>2</sup></p>
+        {#if isActive[index]}
+          <SvelteMarkdown source={project.attributes.Description} options={markdownOptions} />
+        {/if}
+      </div>
+      
+      <div class="sm:w-1/2 xl:w-7/12 sm:pr-6">
+        {#if isActive[index]}
+          {#each project.attributes.Images.data as img}
+            <img class="pb-4" src={img.attributes.url} alt={img.attributes.alternativeText} />
+          {/each}
+        {:else}
+          <img class="project-thumb object-cover" src={project.attributes.Images.data[0].attributes.url} alt={project.attributes.Images.data[0].attributes.alternativeText} />
+        {/if}
+      </div>
+      
     </div>
-    <div class="w-6/12 prose-sm">
-      <h2>{project.attributes.Nom}</h2>
-      <p class="font-semibold">{project.attributes.Localisation}</p>
-      <p class="font-semibold">{project.attributes.Surface} m<sup>2</sup></p>
-      {#if isActive[index]}
-        <SvelteMarkdown source={project.attributes.Description} options={markdownOptions} />
-      {/if}
-    </div>
-  </div>
-{/each}
+  {/each}
+</div>
+
+<style>
+.project-thumb {
+  aspect-ratio: 3/2;
+}
+</style>
