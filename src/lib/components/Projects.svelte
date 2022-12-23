4ea518db6285618990f33e7391ddd60601a807c6
Hendrik Schmidt <hendriknas@gmail.com>
2022-12-23 21:39:57 +0100

    Update Projects.svelte

--- src/lib/components/Projects.svelte
+++ src/lib/components/Projects.svelte
@@ -15,45 +15,35 @@
       }
     };
   });
-  
-  const isActive = projects.map(() => false);
-
-  const makeActive = (index) => {
-    isActive[index] = !isActive[index];
-  }
 
 </script>
 
-<div class="my-36">
+<div>
   {#each projects as project, index (project.id)}
-    <div class="pb-24 flex flex-col sm:flex-row justify-around cursor-pointer" on:click={() => makeActive(index)} on:keypress={() => makeActive(index)} >
+    <div class="pb-20 cursor-pointer">
       
-      <div class="p-2 pr-4 sm:w-1/2 xl:w-5/12 prose prose-sm lg:prose-base">
-        <h2>{project.attributes.Nom}</h2>
-        <p class="font-semibold">{project.attributes.Programme}</p>
-        <p class="font-semibold">{project.attributes.Localisation}</p>
-        <p class="font-semibold">{project.attributes.Surface} m<sup>2</sup></p>
-        {#if isActive[index]}
-          <SvelteMarkdown source={project.attributes.Description} options={markdownOptions} />
-        {/if}
+      <div class="p-3 mb-2 prose sm:w-2/3 lg:w-1/2 border-t-2 border-zinc-900">
+        <h2 class="mb-0.5">{project.attributes.Nom}</h2>
+        <p class="text-color-zinc-400">{project.attributes.Programme}</p>
+        <!-- <p class="font-semibold">{project.attributes.Localisation}</p>
+        <p class="font-semibold">{project.attributes.Surface} m<sup>2</sup></p> -->
+        <!-- <SvelteMarkdown source={project.attributes.Description} options={markdownOptions} /> -->
       </div>
       
-      <div class="sm:w-1/2 xl:w-7/12 sm:pr-6">
-        {#if isActive[index]}
+      <div class="flex w-full overflow-scroll">
           {#each project.attributes.Images.data as img}
-            <img class="pb-4" src={img.attributes.url} alt={img.attributes.alternativeText} />
+          <div class="flex-none w-2/3 sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/5 2xl:w-1/6 mr-8 mb-8">
+            <img class="project-thumb object-cover" src={img.attributes.url} alt={img.attributes.alternativeText} />
+          </div>
           {/each}
-        {:else}
-          <img class="project-thumb object-cover" src={project.attributes.Images.data[0].attributes.url} alt={project.attributes.Images.data[0].attributes.alternativeText} />
-        {/if}
+          <!-- <img class="project-thumb object-cover" src={project.attributes.Images.data[0].attributes.url} alt={project.attributes.Images.data[0].attributes.alternativeText} /> -->
       </div>
-      
     </div>
   {/each}
 </div>
 
 <style>
 .project-thumb {
-  aspect-ratio: 3/2;
+  aspect-ratio: 1/1;
 }
 </style>
