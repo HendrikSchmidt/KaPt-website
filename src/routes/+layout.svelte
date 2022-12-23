e572a1c1afd4c49db0d3d892e67903fe6c634cc2
Hendrik Schmidt <hendriknas@gmail.com>
2022-12-23 20:53:22 +0100

    Update +layout.svelte

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -18,10 +18,10 @@
 </svelte:head>
 
 <div class="app w-screen text-zinc-900">
-  <aside class="fixed top-0 left-0 flex max-sm:flex-col justify-between p-3 w-full z-10">
+  <aside class="fixed top-0 left-0 flex flex-wrap	 justify-between p-3 w-full z-10">
     <a class="logo text-6xl md:text-8xl flex-0" href="{i18n.getLocalizedSlug('home', currentLang)}">KaPt</a>
 
-    <ul class="flex-0 lg:text-2xl font-extralight flex max-sm:justify-around max-sm:w-full align-right">
+    <ul class="flex justify-around max-sm:w-full max-sm:py-2 max-sm:order-last sm:flex-1 sm:justify-end lg:text-2xl">
       <li class="px-2 py-1">
         <NavLink href="{i18n.getLocalizedSlug('philosophy', currentLang)}">
           {i18n.getLocalizedString('philosophy', currentLang)}
@@ -37,27 +37,20 @@
           {i18n.getLocalizedString('contact', currentLang)}
         </NavLink>
       </li>
-      <li class="px-2 py-1 max-sm:hidden">
-        {#if $page.url.pathname.startsWith(`${base}/en`)}
-          <a class="text-zinc-400" href="{i18n.getTranslatedSlug($page.url.pathname, "fr")}">fr</a> / <span>en</span>
-        {:else}
-          <span>fr</span> / <a class="text-zinc-400" href="{i18n.getTranslatedSlug($page.url.pathname, "en")}">en</a>
-        {/if}
-      </li>
     </ul>
+
+    <div class="px-2 py-1 flex-0 p-3 lg:text-2xl z-20" >
+      {#if $page.url.pathname.startsWith(`${base}/en`)}
+        <a class="text-zinc-400" href="{i18n.getTranslatedSlug($page.url.pathname, "fr")}">fr</a> / <span>en</span>
+      {:else}
+        <span>fr</span> / <a class="text-zinc-400" href="{i18n.getTranslatedSlug($page.url.pathname, "en")}">en</a>
+      {/if}
+    </div>
   </aside>
 
-  <main class="sm:pl-48 w-full">
+  <main class="px-4 sm:px-8 w-full">
     <slot />
   </main>
-
-  <div class="sm:hidden fixed top-0 right-0 p-3 sm:text-2xl z-20" >
-    {#if $page.url.pathname.startsWith(`${base}/en`)}
-      <a class="text-zinc-400" href="{i18n.getTranslatedSlug($page.url.pathname, "fr")}">fr</a> / <span>en</span>
-    {:else}
-      <span>fr</span> / <a class="text-zinc-400" href="{i18n.getTranslatedSlug($page.url.pathname, "en")}">en</a>
-    {/if}
-  </div>
 </div>
 
 <style>
