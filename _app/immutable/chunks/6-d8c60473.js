69a3c931e6535c74e2a839650d2104333c919c11
HendrikSchmidt <hendriknas@gmail.com>
2022-12-23 20:08:00 +0000

    Deploying to gh-pages from @ HendrikSchmidt/KaPt-website@c9d2da182a4d81e3bb73a71449764b78deae6b7d 🚀

---
+++ _app/immutable/chunks/6-d8c60473.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/en/philosophy/_page.svelte-1fb5cc93.js";const e=!0;export{t as component,e as server};
---
+++ _app/immutable/chunks/8-d2dd91f1.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/philosophie/_page.svelte-6639cd27.js";const e=!0;export{t as component,e as server};
