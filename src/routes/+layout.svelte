a58147dceb7f9b7b02146829d009edd533a70305
Hendrik Schmidt <hendriknas@gmail.com>
2023-02-03 17:51:40 +0100

    move language switcher position to left

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -18,34 +18,35 @@
 </svelte:head>
 
 <div class="app w-screen text-zinc-900">
-  <aside class="fixed top-0 left-0 flex flex-wrap justify-between p-3 w-full z-10 max-sm:bg-white">
+  <aside class="fixed top-0 left-0 flex flex-wrap justify-between p-4 sm:p-8 w-full z-10 max-sm:bg-white">
     <a class="logo text-6xl md:text-8xl flex-0" href="{i18n.getLocalizedSlug('home', currentLang)}">KaPt</a>
 
-    <ul class="flex justify-around max-sm:w-full max-sm:py-2 max-sm:order-last sm:flex-1 sm:justify-end lg:text-2xl">
-      <li class="px-2 py-1">
-        <NavLink href="{i18n.getLocalizedSlug('philosophy', currentLang)}">
-          {i18n.getLocalizedString('philosophy', currentLang)}
-        </NavLink>
-      </li>
-      <li class="px-2 py-1">
-        <NavLink href="{i18n.getLocalizedSlug('projects', currentLang)}">
-          {i18n.getLocalizedString('projects', currentLang)}
-        </NavLink>
-      </li>
-      <li class="px-2 py-1">
-        <NavLink href="{i18n.getLocalizedSlug('contact', currentLang)}">
-          {i18n.getLocalizedString('contact', currentLang)}
-        </NavLink>
-      </li>
-    </ul>
-
-    <div class="px-2 py-1 flex-0 p-3 lg:text-2xl z-20" >
+    <div class="flex-0 lg:text-2xl z-20 sm:flex-1 text-right">
       {#if $page.url.pathname.startsWith(`${base}/en`)}
         <a class="text-zinc-400" href="{i18n.getTranslatedSlug($page.url.pathname, "fr")}">fr</a> / <span>en</span>
       {:else}
         <span>fr</span> / <a class="text-zinc-400" href="{i18n.getTranslatedSlug($page.url.pathname, "en")}">en</a>
       {/if}
     </div>
+    
+    <ul class="flex justify-between max-sm:w-full max-sm:py-2 max-sm:order-last lg:text-2xl">
+      <li class="sm:pl-4">
+        <NavLink href="{i18n.getLocalizedSlug('philosophy', currentLang)}">
+          {i18n.getLocalizedString('philosophy', currentLang)}
+        </NavLink>
+      </li>
+      <li class="sm:pl-4">
+        <NavLink href="{i18n.getLocalizedSlug('projects', currentLang)}">
+          {i18n.getLocalizedString('projects', currentLang)}
+        </NavLink>
+      </li>
+      <li class="sm:pl-4">
+        <NavLink href="{i18n.getLocalizedSlug('contact', currentLang)}">
+          {i18n.getLocalizedString('contact', currentLang)}
+        </NavLink>
+      </li>
+    </ul>
+    
   </aside>
 
   <main class="mt-40 p-4 sm:p-8 w-full flex flex-col items-center">
