c62141c6167feb00c1aec64db1d5cc9ed241b7e8
Hendrik Schmidt <hendriknas@gmail.com>
2023-02-09 17:57:36 +0100

    change title casing

--- src/lib/components/Contact.svelte
+++ src/lib/components/Contact.svelte
@@ -9,7 +9,7 @@
 
 <div class="prose w-full prose-sm lg:prose-base xl:prose-lg">
   
-  <h1 class="uppercase">{i18n.getLocalizedString('contact', lang)}</h1>
+  <h1>{i18n.getLocalizedString('contact', lang)}</h1>
   <div class="flex w-full">
     <div class="text-zinc-400 lowercase w-1/2">
       <p>{i18n.getLocalizedString('mail', lang)}</p>
@@ -22,7 +22,7 @@
       <p>{contact.Adresse}</p>
     </div>
   </div>
-  <h2 class="lowercase font-normal">{i18n.getLocalizedString('social', lang)}</h2>
+  <h2>{i18n.getLocalizedString('social', lang)}</h2>
   <div class="flex">
     <div class="text-zinc-400 lowercase w-1/2">
       <p>instagram</p>
