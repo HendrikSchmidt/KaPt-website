9617bec7540daf1a191f1c06c57930e9d4f413d4
Hendrik Schmidt <hendriknas@gmail.com>
2023-02-10 01:06:22 +0100

    clean up

--- src/lib/components/Projects.svelte
+++ src/lib/components/Projects.svelte
@@ -6,17 +6,7 @@
   export let projects;
   export let lang;
 
-  projects = projects.map(project => {
-    let shortDescription = project.attributes.Description.slice(0, 500);
-    shortDescription = `${shortDescription.slice(0, shortDescription.lastIndexOf(' '))} ...`;
-    return {
-      ...project,
-      attributes: {
-        ...project.attributes,
-        shortDescription
-      }
-    };
-  });
+  let sizes = "(max-width: 639px) 90vw, (max-width: 767px) 75vw, (max-width: 1023px) 50vw, (max-width: 1279px) 40vw, (max-width: 1535px) 30vw, 25vw";
 </script>
 
 <div>
@@ -25,9 +15,6 @@
       
       <div class="py-3 mb-5 prose sm:w-2/3 lg:w-1/2 border-b-2 border-zinc-900">
         <h2 class="font-normal">{project.attributes.Nom}</h2>
-        <!-- <p class="font-semibold">{project.attributes.Localisation}</p>
-        <p class="font-semibold">{project.attributes.Surface} m<sup>2</sup></p> -->
-        <!-- <SvelteMarkdown source={project.attributes.Description} options={markdownOptions} /> -->
       </div>
       
       <div class="flex w-full overflow-scroll">
@@ -35,12 +22,7 @@
           <div class="flex-none w-2/3 sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/5 2xl:w-1/6 mr-8 mb-8 overflow-hidden">
             <Image
               img={img.attributes}
-              sizes="(max-width: 639px) 90vw,
-                    (max-width: 767px) 75vw,
-                    (max-width: 1023px) 50vw,
-                    (max-width: 1279px) 40vw,
-                    (max-width: 1535px) 30vw,
-                    25vw"
+              sizes={sizes}
               src="small"
               lazy={index > 2 || imgIndex > 6}
               classString="aspect-square object-cover saturate-0 hover:saturate-100"
@@ -52,12 +34,7 @@
             <div class="flex-none w-2/3 sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/5 2xl:w-1/6 mr-8 mb-8 overflow-hidden">
               <Image
                 img={plan.attributes}
-                sizes="(max-width: 639px) 90vw,
-                      (max-width: 767px) 75vw,
-                      (max-width: 1023px) 50vw,
-                      (max-width: 1279px) 40vw,
-                      (max-width: 1535px) 30vw,
-                      25vw"
+                sizes={sizes}
                 src="small"
                 lazy={index > 2 || project.attributes.Images.data.length + planIndex > 6 }
                 classString="aspect-square object-cover saturate-0 hover:saturate-100"
