a855b5fcbcb12c9a78dd583f40d5e35770ac38ed
HendrikSchmidt <hendriknas@gmail.com>
2023-05-07 16:30:45 +0000

    Deploying to gh-pages from @ HendrikSchmidt/KaPt-website@e524bb407c9df1eef7f14ff62b5b55b6db2e0e0f 🚀

--- _app/immutable/chunks/stores.134b2e45.js
+++
@@ -1,1 +0,0 @@
-import"./index.2baf3ca1.js";import{s as e}from"./singletons.23df0682.js";const r=()=>{const s=e;return{page:{subscribe:s.page.subscribe},navigating:{subscribe:s.navigating.subscribe},updated:s.updated}},b={subscribe(s){return r().page.subscribe(s)}};export{b as p};
