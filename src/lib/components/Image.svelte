42d51a9c38f7a9e4a37abfd22274f0fadee3bea5
Hendrik Schmidt <hendriknas@gmail.com>
2023-05-15 14:42:26 -0500

    format

--- src/lib/components/Image.svelte
+++ src/lib/components/Image.svelte
@@ -1,41 +1,40 @@
 <script>
-    export let lazy = false;
-    export let img;
-    export let src;
-    export let classString = "";
-    export let sizes = "100vw";
-    export let pictureRelative = true;
+	export let lazy = false;
+	export let img;
+	export let src;
+	export let classString = '';
+	export let sizes = '100vw';
+	export let pictureRelative = true;
 
-    $: formats = img.formats;
-    const scales = {
-        small: '500w',
-        medium: '750w',
-        large: '1000w',
-        xlarge: '1500w',
-    };
-    const replaceJPGWithWebP = url => url.replace(/\.jpe?g/gi, '.webp');
-    $: srcset = Object.entries(scales).filter(([scale]) => formats[scale]).map(([scale, width]) => `${formats[scale].url} ${width}`).join();
-    srcset += `, ${img.url} 2000w`;
+	$: formats = img.formats;
+	const scales = {
+		small: '500w',
+		medium: '750w',
+		large: '1000w',
+		xlarge: '1500w'
+	};
+	const replaceJPGWithWebP = (url) => url.replace(/\.jpe?g/gi, '.webp');
+	$: srcset = Object.entries(scales)
+		.filter(([scale]) => formats[scale])
+		.map(([scale, width]) => `${formats[scale].url} ${width}`)
+		.join();
+	srcset += `, ${img.url} 2000w`;
 </script>
 
-<picture class={pictureRelative && "relative"}>
-  <source
-    type="image/webp"
-    srcset={replaceJPGWithWebP(srcset)}
-    sizes={sizes}
-  />
-  <img
-    class="w-full h-full object-center transition duration-1000 transform hover:scale-110 {classString}"
-    loading={lazy ? 'lazy' : 'eager'}
-    src={formats[src] ? formats[src].url : formats.small.url}
-    alt={img.alternativeText}
-    on:click
-  />
-  <p class="z-20 opacity-0 transition-all text-xs absolute bottom-0 px-1 bg-white">{img.caption}</p>
+<picture class={pictureRelative && 'relative'}>
+	<source type="image/webp" srcset={replaceJPGWithWebP(srcset)} {sizes} />
+	<img
+		class="w-full h-full object-center transition duration-1000 transform hover:scale-110 {classString}"
+		loading={lazy ? 'lazy' : 'eager'}
+		src={formats[src] ? formats[src].url : formats.small.url}
+		alt={img.alternativeText}
+		on:click
+	/>
+	<p class="z-20 opacity-0 transition-all text-xs absolute bottom-0 px-1 bg-white">{img.caption}</p>
 </picture>
 
 <style>
-  picture:hover p {
-    opacity: 1;
-  }
+	picture:hover p {
+		opacity: 1;
+	}
 </style>
