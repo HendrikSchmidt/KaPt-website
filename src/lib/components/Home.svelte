478faac1c0d45d9390b3231c858c6dddb467765b
Hendrik Schmidt <hendriknas@gmail.com>
2023-11-30 00:03:29 +0100

    only display content on home if available

--- src/lib/components/Home.svelte
+++ src/lib/components/Home.svelte
@@ -31,20 +31,22 @@
 </script>
 
 <div class="w-full overflow-hidden landing-image relative aspect-[4/3] md:aspect-[3/2]">
-  <a
-    href="{pages[currentImageIndex].url}"
-  >
-    {#each pages as page, i}
-      <Image
-        img={page.attributes.Images.data[0].attributes}
-        sizes="150wv"
-        src="xlarge"
-        lazy={i > 0}
-        pictureRelative={false}
-        classString="absolute object-cover saturate-0 hover:saturate-100 {i === currentImageIndex
-          ? 'opacity-100 z-10'
-          : 'opacity-0 z-0'}"
-      />
-    {/each}
-  </a>
+  {#if pages.length}
+    <a
+      href="{pages[currentImageIndex].url}"
+    >
+      {#each pages as page, i}
+        <Image
+          img={page.attributes.Images.data[0].attributes}
+          sizes="150wv"
+          src="xlarge"
+          lazy={i > 0}
+          pictureRelative={false}
+          classString="absolute object-cover saturate-0 hover:saturate-100 {i === currentImageIndex
+            ? 'opacity-100 z-10'
+            : 'opacity-0 z-0'}"
+        />
+      {/each}
+    </a>
+  {/if}
 </div>
