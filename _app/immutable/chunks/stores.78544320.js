67c1783430b91244720fe3b68a9f33e9f73e0f06
HendrikSchmidt <HendrikSchmidt@users.noreply.github.com>
2024-04-06 19:28:38 +0000

    Deploying to gh-pages from @ HendrikSchmidt/KaPt-website@a5f131471eeaabf9c89dab505b3d0e85ebb85d96 🚀

---
+++ _app/immutable/chunks/stores.78544320.js
@@ -0,0 +1,1 @@
+import{d as e}from"./singletons.f1c6dd91.js";const r=()=>{const s=e;return{page:{subscribe:s.page.subscribe},navigating:{subscribe:s.navigating.subscribe},updated:s.updated}},b={subscribe(s){return r().page.subscribe(s)}};export{b as p};
