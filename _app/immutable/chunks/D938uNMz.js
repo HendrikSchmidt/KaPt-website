3cd2583ff7e9e7cdfe0bb1b71f3ae0a8194df44c
HendrikSchmidt <HendrikSchmidt@users.noreply.github.com>
2025-03-20 12:00:31 +0000

    Deploying to gh-pages from @ HendrikSchmidt/KaPt-website@01e65d991fca921a21e99dbc1fd3f69e9bf723ca 🚀

--- _app/immutable/chunks/D938uNMz.js
+++
@@ -1,1 +0,0 @@
-import{s as e}from"./QKYR6pG7.js";const r=()=>{const s=e;return{page:{subscribe:s.page.subscribe},navigating:{subscribe:s.navigating.subscribe},updated:s.updated}},b={subscribe(s){return r().page.subscribe(s)}};export{b as p};
